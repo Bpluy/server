9e8cfb992c26a5dfcebabd4e63f52f2c2da356b6
Bpluy <36358690+Bpluy@users.noreply.github.com>
2024-05-03 12:10:10 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import socket
+from functions import *
+
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+IP = socket.gethostbyname(socket.gethostname())
+print(IP)
+PORT = 11333
+listener.bind((IP,PORT))
+listener.listen(0)
+
+#connection, address = listener.accept()
+
+#connection.send("Подключение успешно!".encode('utf8'))
+
+while True:
+    connection, address = listener.accept()
+    connection.send("Successfull".encode('utf8'))
+    request = connection.recv(1024).decode("utf8").split(' ')
+    answer = RequestHandling(request)
+    connection.send(answer.encode('utf8'))
+    connection.close()
